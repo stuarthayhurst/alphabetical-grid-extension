34c02d369690943061c56d01889a106f2245872c
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-05-11 16:42:25 +0100

    Made single and double quotes consistent

--- extension.js
+++ extension.js
@@ -16,13 +16,13 @@ function disable() {
 class Extension {
   constructor() {
     //Load gsettings values for GNOME Shell
-    this.shellSettings = ExtensionUtils.getSettings("org.gnome.shell");
+    this.shellSettings = ExtensionUtils.getSettings('org.gnome.shell');
   }
 
   _reorderGrid() {
     //Alphabetically order the grid, by resetting the gsettings value for 'app-picker-layout'
     if (this.shellSettings.is_writable('app-picker-layout')) {
-      this.shellSettings.reset("app-picker-layout");
+      this.shellSettings.reset('app-picker-layout');
       this._logMessage('Reordered grid');
     } else {
       this._logMessage('org.gnome.shell app-picker-layout in unwritable, skipping reorder')
@@ -30,17 +30,17 @@ class Extension {
   }
 
   _logMessage(message) {
-    log("alphabetical-app-grid: " + message);
+    log('alphabetical-app-grid: ' + message);
   }
 
   waitForExternalReorder() {
     //Connect to gsettings and wait for the order to change
-    this.reorderSignal = this.shellSettings.connect("changed::app-picker-layout", () => {
+    this.reorderSignal = this.shellSettings.connect('changed::app-picker-layout', () => {
       //Work out if the change was internal or external
       let appLayout = this.shellSettings.get_value('app-picker-layout');
-      if (appLayout.recursiveUnpack() != "") {
+      if (appLayout.recursiveUnpack() != '') {
         //When an external change is picked up, reorder the grid
-        this._logMessage("App grid layout changed, triggering reorder");
+        this._logMessage('App grid layout changed, triggering reorder');
         this._reorderGrid();
       }
     });
