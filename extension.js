8a25876fe408f8d302d0c7aa5e06e070a834e76f
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-05-25 21:15:23 +0100

    Reorder folder contents when app grid order changes (Closes #1)

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-const {GLib} = imports.gi;
+const {GLib, Gio, Shell} = imports.gi;
 const Main = imports.ui.main;
 const Config = imports.misc.config;
 const ExtensionUtils = imports.misc.extensionUtils;
@@ -34,13 +34,56 @@ class Extension {
     this._appDisplay = Main.overview._overview._controls._appDisplay;
     //Get GNOME shell version
     this.shellVersion = Number.parseInt(Config.PACKAGE_VERSION.split('.'));
+    //Create AppSystem
+    this.appSystem = new Shell.AppSystem();
   }
 
   _logMessage(message) {
     log('alphabetical-app-grid: ' + message);
   }
 
+  //Called by reorderGrid()
+  reorderFolderContents() {
+    this._logMessage(_('Reordering folder contents'));
+
+    //Get array of folders from 'folder-children' key
+    let folderArray = this.folderSettings.get_value('folder-children').get_strv();
+
+    //Loop through all folders, and reorder their contents
+    folderArray.forEach((targetFolder, i) => {
+      //Get the contents of the folder, from gsettings value
+      let folderContentsSettings = Gio.Settings.new_with_path('org.gnome.desktop.app-folders.folder', '/org/gnome/desktop/app-folders/folders/' + targetFolder + '/');
+      let folderContents = folderContentsSettings.get_value('apps').get_strv();
+
+      //Loop through detected folders and get display names for their contents
+      let numRemovedApps = 0;
+      folderContents.forEach((folderApp, i) => {
+        //Lookup display name of each app
+        let appInfo = this.appSystem.lookup_app(folderApp);
+        if (appInfo != null) {
+          folderContents[i - numRemovedApps] = new String(appInfo.get_name());
+          folderContents[i - numRemovedApps].desktopFile = folderApp;
+        } else { //App doesn't exist, so remove from array
+          folderContents = folderContents.filter(element => element != folderApp);
+          numRemovedApps += 1;
+        }
+      });
+
+      //Alphabetically sort the folder's contents, by the display name
+      folderContents.sort();
+
+      //Replace each element with the app's .desktop filename
+      folderContents.forEach((folderApp, i) => { folderContents[i] = folderApp.desktopFile; });
+
+      //Set the gsettings value for 'apps' to the ordered list
+      folderContentsSettings.set_value('apps', new GLib.Variant('as', folderContents));
+    });
+  }
+
   reorderGrid() {
+    //Alphabetically order the contents of each folder
+    this.reorderFolderContents();
+
     //Alphabetically order the grid, by blanking the gsettings value for 'app-picker-layout'
     if (this.shellSettings.is_writable('app-picker-layout')) {
       //Change gsettings value
