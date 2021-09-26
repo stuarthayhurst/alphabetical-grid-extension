b18c65c6656126dc2028e5618df1404fce0a0777
stuarthayhurst <stuart.a.hayhurst@gmail.com>
2021-09-26 10:39:31 +0100

    Reduced size of scripts and moved credit attribution to README

--- scripts/update-po.sh
+++ scripts/update-po.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#This script is derived from the awesome Fly-Pie Project (https://github.com/Schneegans/Fly-Pie), originally licensed under the MIT license (https://github.com/Schneegans/Fly-Pie/blob/develop/LICENSE)
-#Modifications were made in order to make the script work with this repository
 #This script takes 'po/messages.pot' and compiles the latest '.po' file(s) from it
 #Usage: 'update-po.sh -l <LANG-CODE>', use '-a' to update all '.po' files
 
@@ -18,33 +16,28 @@ promptNewTranslation() {
   fi
 }
 
-if ! command -v msgmerge &> /dev/null; then
-  echo -e "\nERROR: Could not find msgmerge"; exit 1
-fi
+#Change to repository root and exit on failure
+set -e
+cd "$( cd "$( dirname "$0" )" && pwd )/.." || exit 1
 
-#Change to repository root
-cd "$( cd "$( dirname "$0" )" && pwd )/.." || \
-  { echo -e "\nERROR: Could not find the repository root"; exit 1; }
-
-if [[ "$1" == "-l" ]]; then #Update/Create one specific '.po' file
+if [[ "$1" == "-l" ]]; then #Update / create one specific '.po' file
   #Check if a valid language code was given
   if [[ -f "po/$2.po" ]]; then
     echo -n "Updating '$2.po'"
     msgmerge --previous -U "po/$2.po" po/messages.pot
 
-    #Output translation progress
     echo -n "  "; msgfmt --check --verbose --output-file=/dev/null "po/$2.po"
   else
     promptNewTranslation "$2"
   fi
 elif [[ "$1" == "-a" ]]; then #Update all '.po' files
-  for FILE in po/*.po; do
+  for file in po/*.po; do
     #Handle no .po files
-    [[ -e "$FILE" ]] || { echo -e "\nERROR: No .po files found, exiting."; exit 1; }
-    echo -n "Updating '$FILE'"
-    msgmerge --previous -U "$FILE" po/messages.pot
-    #Output translation progress
-    echo -n "  "; msgfmt --check --verbose --output-file=/dev/null "$FILE"
+    [[ -e "$file" ]] || { echo -e "\nERROR: No .po files found, exiting."; exit 1; }
+    echo -n "Updating '$file'"
+    msgmerge --previous -U "$file" po/messages.pot
+
+    echo -n "  "; msgfmt --check --verbose --output-file=/dev/null "$file"
   done
 else
   echo "ERROR: You need to specify a flag!"; exit 1
--- scripts/update-pot.sh
+++ scripts/update-pot.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-#This script is derived from the awesome Fly-Pie Project (https://github.com/Schneegans/Fly-Pie), originally licensed under the MIT license (https://github.com/Schneegans/Fly-Pie/blob/develop/LICENSE)
-#Modifications were made in order to make the script work with this repository
+#This script scans the source code for any translatable strings and updates the po/messages.pot file accordingly
 
-# This script scans the source code for any translatable strings and updates the po/messages.pot file accordingly
-
-#Exit on failure
+#Change to repository root and exit on failure
 set -e
+cd "$( cd "$( dirname "$0" )" && pwd )/.." || exit 1
 
-if ! command -v xgettext &> /dev/null; then
-  echo -e "\nERROR: Could not find xgettext"; exit 1
-fi
-
-#Change to repository root
-cd "$( cd "$( dirname "$0" )" && pwd )/.." || \
-  { echo -e "\nERROR: Could not find the repository root"; exit 1; }
-
-#Update the template file with the strings from the source tree
+#Update the template file with the strings from the source files
 echo "Generating 'messages.pot'..."
 xgettext --from-code=UTF-8 \
          --add-comments=Translators \
@@ -24,7 +14,7 @@ xgettext --from-code=UTF-8 \
          --output=po/messages.pot \
          -- *.js lib/*.js ui/*.ui
 
-#Replace some lines of the header with our own.
+#Replace some lines of the header with our own
 sed -i '1s/.*/# <LANGUAGE> translation for the Alphabetical App Grid GNOME Shell Extension./' po/messages.pot
 sed -i "2s/.*/# Copyright (C) $(date +%Y) Stuart Hayhurst/" po/messages.pot
 sed -i '17s/CHARSET/UTF-8/' po/messages.pot
