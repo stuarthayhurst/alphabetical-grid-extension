9441fcca224147a21c740bd356f3cc18a116f539
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-08-21 22:35:40 +0100

    Replace UI with libadwaita

--- extension/prefs.js
+++ extension/prefs.js
@@ -1,174 +1,126 @@
 /* exported AppGridPrefs */
 
 //Main imports
-import Gtk from 'gi://Gtk';
 import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
+import Gtk from 'gi://Gtk';
 import Adw from 'gi://Adw';
+import GObject from 'gi://GObject';
 
 //Extension system imports
 import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-var PrefsPages = class PrefsPages {
-  constructor(path, uuid, version, settings) {
-    this._settings = settings;
-    this._path = path;
-    this._version = version;
-
-    this._builder = new Gtk.Builder();
-    this._builder.set_translation_domain(uuid);
-
-    this._createPreferences();
-    this._createAbout();
-    this._createCredits();
-  }
-
-  _getCredits(creditDataRaw, creditType) {
-    let creditsData = JSON.parse(creditDataRaw);
-    let creditStrings = [];
-    creditsData[creditType].forEach((creditUser) => {
-      //Build the credit string for each individual
-      let string = creditUser['name'];
-
-      //If given, use the email as a clickable link
-      if (creditUser['contact'] != '') {
-        string = '<a href="mailto::' + creditUser['contact'] + '">' + string + '</a>'
-      }
-
-      //Use the year if given
-      if (creditUser['year'] != '' && creditUser['year'] != undefined) {
-        string = string + ' ' + creditUser['year'];
-      }
-
-      creditStrings.push(string);
+var PrefsPage = GObject.registerClass(
+class PrefsPage extends Adw.PreferencesPage {
+  _init(pageInfo, groupsInfo, settingsInfo, settings) {
+    super._init({
+      title: pageInfo[0],
+      icon_name: pageInfo[1]
     });
 
-    return creditStrings;
+    this._extensionSettings = settings;
+    this._settingGroups = {};
+    this._settingRows = {};
+
+    //Setup settings
+    this._createGroups(groupsInfo);
+    this._createSettings(settingsInfo);
   }
 
-  _createPreferences() {
-    this._builder.add_from_file(this._path + '/gtk4/prefs.ui');
+  _createGroups(groupsInfo) {
+    //Store groups, set title and add to window
+    groupsInfo.forEach((groupInfo) => {
+      this._settingGroups[groupInfo[0]] = new Adw.PreferencesGroup();
+      this._settingGroups[groupInfo[0]].set_title(groupInfo[1]);
+      this.add(this._settingGroups[groupInfo[0]]);
+    });
+  }
 
-    //Get the settings container widget
-    this.preferencesWidget = this._builder.get_object('main-prefs');
-
-    this.settingElements = {
-      'sort-folders-switch': {
-        'settingKey': 'sort-folder-contents',
-        'bindProperty': 'active'
-      },
-      'folder-order-dropdown': {
-        'settingKey': 'folder-order-position',
-        'bindProperty': 'active-id'
-      },
-      'logging-enabled-switch': {
-        'settingKey': 'logging-enabled',
-        'bindProperty': 'active'
+  _createSettings(settingsInfo) {
+    settingsInfo.forEach(settingInfo => {
+      //Check the target group exists
+      if (!(settingInfo[0] in this._settingGroups)) {
+        return;
       }
-    }
 
-    //Loop through settings toggles and dropdowns and bind together
-    Object.keys(this.settingElements).forEach((element) => {
-      this._settings.bind(
-        this.settingElements[element].settingKey, //GSettings key to bind to
-        this._builder.get_object(element), //GTK UI element to bind to
-        this.settingElements[element].bindProperty, //The property to share
+      //Create a row with a switch, title and subtitle
+      let settingRow = new Adw.SwitchRow({
+        title: settingInfo[2],
+        subtitle: settingInfo[3]
+      });
+
+      //Connect the switch to the setting
+      this._extensionSettings.bind(
+        settingInfo[1], //GSettings key to bind to
+        settingRow, //Object to bind to
+        'active', //The property to share
         Gio.SettingsBindFlags.DEFAULT
       );
+
+      //Add the row to the group, and save for later
+      this._settingGroups[settingInfo[0]].add(settingRow);
+      this._settingRows[settingInfo[1]] = settingRow;
     });
   }
 
-  _createAbout() {
-    this._builder.add_from_file(this._path + '/gtk4/about.ui');
+  addLinks(window, linksInfo, groupName) {
+    //Setup and add links group to window
+    let linksGroup = new Adw.PreferencesGroup();
+    linksGroup.set_title(groupName);
+    this.add(linksGroup);
 
-    //Get the about page and fill in values
-    this.aboutWidget = this._builder.get_object('about-page');
-    this._builder.get_object('extension-version').set_label('v' + this._version.toString());
-    this._builder.get_object('extension-icon').set_from_file(this._path + '/icon.svg');
+    linksInfo.forEach((linkInfo) => {
+      //Create a row for the link widget
+      let linkEntryRow = new Adw.ActionRow({
+        title: linkInfo[0],
+        subtitle: linkInfo[1],
+        activatable: true
+      });
+
+      //Open the link when clicked
+      linkEntryRow.connect('activated', () => {
+        let uriLauncher = new Gtk.UriLauncher();
+        uriLauncher.set_uri(linkInfo[2]);
+        uriLauncher.launch(window, null, null);
+      });
+
+      linksGroup.add(linkEntryRow);
+    });
   }
-
-  _createCredits() {
-    this._builder.add_from_file(this._path + '/gtk4/credits.ui');
-
-    //Set the icon
-    this._builder.get_object('extension-credits-icon').set_from_file(this._path + '/icon.svg');
-
-    //Get the credits page and grid
-    this.creditsWidget = this._builder.get_object('credits-page');
-    let creditsGrid = this._builder.get_object('credits-grid');
-
-    //Read in the saved extension credits
-    let [success, data] = GLib.file_get_contents(this._path + '/credits.json');
-    if (!success) {
-      return;
-    }
-
-    //Decode the file's contents
-    data = new TextDecoder().decode(data);
-
-    //Parse the credits
-    let developerStrings = this._getCredits(data, 'developers');
-    let translatorStrings = this._getCredits(data, 'translators');
-
-    //Set the target number of rows to the required amount
-    let developerCount = developerStrings.length;
-    let translatorCount = translatorStrings.length;
-    let rowCount = developerCount > translatorCount ? developerCount : translatorCount;
-
-    //Add the credit and a separator for each row
-    for (let i = 0; i < rowCount; i++) {
-      let baseRow = i * 2
-      //Append a row for credits
-      creditsGrid.insert_row(baseRow + 2);
-
-      //If there are developers left to append, do it
-      if (developerCount > i) {
-        let developerLabel = new Gtk.Label();
-        developerLabel.set_markup(developerStrings[i]);
-        creditsGrid.attach(developerLabel, 0, baseRow + 2, 1, 1);
-      }
-
-      //If there are translators left to append, do it
-      if (translatorCount > i) {
-        let translatorLabel = new Gtk.Label();
-        translatorLabel.set_markup(translatorStrings[i]);
-        creditsGrid.attach(translatorLabel, 1, baseRow + 2, 1, 1);
-      }
-
-      //Add a separator
-      creditsGrid.insert_row(baseRow + 3);
-      creditsGrid.attach(new Gtk.Separator(), 0, baseRow + 3, 2, 1);
-    }
-  }
-}
+});
 
 export default class AppGridPrefs extends ExtensionPreferences {
   //Create preferences window with libadwaita
   fillPreferencesWindow(window) {
-    //Create pages and widgets
-    let prefsPages = new PrefsPages(this.path, this.uuid,
-                                    this.metadata.version,
-                                    this.getSettings());
+    //Translated title, icon name
+    let pageInfo = [_('Settings'), 'preferences-system-symbolic'];
 
-    let pageInfos = [
-      //Title, icon, widget
-      [_('Settings'), 'preferences-system-symbolic', prefsPages.preferencesWidget],
-      [_('About'), 'help-about-symbolic', prefsPages.aboutWidget],
-      [_('Credits'), 'system-users-symbolic', prefsPages.creditsWidget]
+    let groupsInfo = [
+      //Group ID, translated title
+      ['general', _('General settings')],
+      ['developer', _('Developer settings')]
     ];
 
-    pageInfos.forEach((pageInfo) => {
-      let page = new Adw.PreferencesPage();
-      let group = new Adw.PreferencesGroup();
+    let settingsInfo = [
+      //Group ID, setting key, title, subtitle
+      ['general', 'sort-folder-contents', _('Sort folder contents'), _('')],
+      ['general', 'folder-order-position',  _('Position of ordered folders'), _('')],
+      ['developer', 'logging-enabled-switch',  _('Enable extension logging'), _('')]
+    ];
 
-      //Build the group and page
-      page.set_title(pageInfo[0]);
-      page.set_icon_name(pageInfo[1]);
-      group.add(pageInfo[2]);
-      page.add(group);
+    //Create settings page from info
+    let settingsPage = new PrefsPage(pageInfo, groupsInfo, settingsInfo, this.getSettings());
 
-      //Add to the window
-      window.add(page);
-    });
+    //Define and add links
+    let linksInfo = [
+      //Translated title, link
+      [_('Report an issue'), _('GitHub issue tracker'), 'https://github.com/stuarthayhurst/alphabetical-grid-extension/issues'],
+      [_('Donate via GitHub'), _('Become a sponsor'), 'https://github.com/sponsors/stuarthayhurst'],
+      [_('Donate via PayPal'), _('Thanks for your support :)'), 'https://www.paypal.me/stuartahayhurst']
+    ];
+    settingsPage.addLinks(window, linksInfo, _("Links"));
+
+    //Add the pages to the window, enable searching
+    window.add(settingsPage);
+    window.set_search_enabled(true);
   }
 }
